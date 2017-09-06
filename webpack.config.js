2815f2f52b07919abaf184c5f7f7ab81c1c0305c
Nick Pray <nick.pray@imail.org>
2017-09-06 08:15:50 -0600

    Adds filename to webpack output config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var path = require('path');
 module.exports = {
 	entry: './src/index.ts',
 	output:{
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
 	},
 	resolve:{
 		extensions:['.ts']
